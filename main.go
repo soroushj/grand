8b181b31cce13ea0093183400f452d151199582e
Soroush Javadi <soroush.javadi@gmail.com>
2024-03-29 20:37:28 +0330

    add package doc comment

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
+// Grand generates cryptographically-secure random byte strings.
+//
+// Usage:
+//
+//	grand [flags]
+//
+// The flags are:
+//
+//	-e encoding
+//	    output encoding; one of:
+//	      "hex"   - base16
+//	      "b64s"  - base64, standard alphabet
+//	      "b64sr" - base64, standard alphabet, no padding
+//	      "b64u"  - base64, url safe alphabet
+//	      "b64ur" - base64, url safe alphabet, no padding
+//	      "b32s"  - base32, standard alphabet
+//	      "b32sr" - base32, standard alphabet, no padding
+//	      "b32h"  - base32, extended hex alphabet
+//	      "b32hr" - base32, extended hex alphabet, no padding
+//	      (default "hex")
+//	-n int
+//	    number of random byte strings to generate (default 1)
+//	-s size
+//	    size of random byte strings; an integer or an inclusive range,
+//	    e.g. "16-32" (if a range is specified, the size of each byte
+//	    string will be a cryptographically-secure random number in the
+//	    range) (default "16")
 package main
 
 import (
