a4b366c03da5f26a2f5e8951e7f89d725bbd3bd1
Soroush Javadi <soroush.javadi@gmail.com>
2024-04-03 20:10:59 +0330

    e2e test: change test case structure

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main_test
 
 import (
+	"encoding/base32"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -11,28 +15,43 @@ import (
 func TestGrand(t *testing.T) {
 	bin := os.Getenv("BIN")
 	testCases := []struct {
-		name           string
-		args           []string
-		exitCode       int
-		validateStdout func([]string) error
+		n        string
+		e        string
+		s        string
+		sMin     int
+		sMax     int
+		exitCode int
 	}{
-		{"defaults", []string{}, 0, func(r []string) error {
-			if len(r) != 1 {
-				return fmt.Errorf("rands num: got %v want 1", len(r))
-			}
-			if len(r[0]) != 32 {
-				return fmt.Errorf("rand len: got %v want 32", len(r[0]))
-			}
-			return nil
-		}},
+		{n: "0", exitCode: 2},
+		{n: "1", e: "hex", s: "16", sMin: 16, sMax: 16, exitCode: 0},
 	}
+	decoders := map[string]func([]byte, []byte) (int, error){
+		"hex":   hex.Decode,
+		"b64s":  base64.StdEncoding.Decode,
+		"b64sr": base64.RawStdEncoding.Decode,
+		"b64u":  base64.URLEncoding.Decode,
+		"b64ur": base64.RawURLEncoding.Decode,
+		"b32s":  base32.StdEncoding.Decode,
+		"b32sr": base32.StdEncoding.WithPadding(base32.NoPadding).Decode,
+		"b32h":  base32.HexEncoding.Decode,
+		"b32hr": base32.HexEncoding.WithPadding(base32.NoPadding).Decode,
+	}
+	maxSize := 0
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			out, err := exec.Command(bin, tc.args...).Output()
+		if tc.sMax > maxSize {
+			maxSize = tc.sMax
+		}
+	}
+	discard := make([]byte, maxSize)
+	for _, tc := range testCases {
+		name := fmt.Sprintf("e=%v,s=%v,n=%v", tc.e, tc.s, tc.n)
+		t.Run(name, func(t *testing.T) {
+			cmd := exec.Command(bin, "-e", tc.e, "-s", tc.s, "-n", tc.n)
+			out, err := cmd.Output()
 			exitCode := 0
 			if err != nil {
 				if exitErr, ok := err.(*exec.ExitError); !ok {
-					t.Fatalf("unknown error type: %v", err)
+					t.Fatalf("non-exit error: %v", err)
 				} else {
 					exitCode = exitErr.ExitCode()
 				}
@@ -40,13 +59,40 @@ func TestGrand(t *testing.T) {
 			if exitCode != tc.exitCode {
 				t.Fatalf("exit code: got %v want %v", exitCode, tc.exitCode)
 			}
-			if tc.validateStdout == nil {
-				t.SkipNow()
+			if exitCode != 0 {
+				if len(out) != 0 {
+					t.Fatalf("stdout: got %q want empty", string(out))
+				} else {
+					t.SkipNow()
+				}
 			}
-			s := strings.TrimSuffix(string(out), "\n")
-			r := strings.Split(s, "\n")
-			if err := tc.validateStdout(r); err != nil {
-				t.Errorf("invalid stdout: %v; stdout:\n%v", err, s)
+			rs := strings.Split(string(out), "\n")
+			if len(rs) == 0 {
+				t.Fatalf("empty stdout")
+			}
+			if rs[len(rs)-1] != "" {
+				t.Errorf("stdout does not end with a newline")
+			} else {
+				rs = rs[:len(rs)-1]
+			}
+			n, err := strconv.Atoi(tc.n)
+			if err != nil {
+				t.Fatalf("invalid test case: invalid n: %q", tc.n)
+			}
+			if len(rs) != n {
+				t.Errorf("num of rands: got %v want %v", len(rs), tc.n)
+			}
+			decode, ok := decoders[tc.e]
+			if !ok {
+				t.Fatalf("invalid test case: invalid e: %q", tc.e)
+			}
+			for _, r := range rs {
+				sd, err := decode(discard, []byte(r))
+				if err != nil {
+					t.Errorf("rand %q: decoding: %v", r, err)
+				} else if sd < tc.sMin || sd > tc.sMax {
+					t.Errorf("rand %q: size: got %v want %v-%v", r, sd, tc.sMin, tc.sMax)
+				}
 			}
 		})
 	}
